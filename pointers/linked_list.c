f895ffd08b7348a5a970ea146c54005d23048414
stackiller <david.vegasc@gmail.com>
2019-06-18 16:37:12 -0300

    .

--- pointers/linked_list.c
+++ pointers/linked_list.c
@@ -13,7 +13,7 @@ not necessary to make the declaration of a predefined size, even in the case of 
 Using linked lists besides offering more flexibility helps a lot when working with asynchronous
 data that need to be arranged in a sequential xD list.
 
-Author: aSynKronus
+Author: stackiller
 
 */
 
